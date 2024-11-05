34b3ab3feb2ed011241bcf2b57469d05ba0f8e98
Mina <153019844+minarizky@users.noreply.github.com>
2024-11-05 14:08:55 -0500

    Delete routes directory

--- routes/bookRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { searchBooks, getFavorites, addFavorite } = require('../controllers/bookController');
-const router = express.Router();
-
-router.get('/search', searchBooks);
-router.get('/favorites', getFavorites);
-router.post('/favorites', addFavorite);
-
-module.exports = router;
